610e07bddcff81f3a97f7194a22a2c9f7d6e6148
lockmx <thiagoalves.f97@gmail.com>
2022-07-30 00:16:21 -0300

    eu

---
+++ script.js
@@ -0,0 +1,12 @@
+let ligar = document.querySelector('#lampada');
+let botao = document.querySelector("#botao");
+
+
+botao.addEventListener('click' , function () {
+    
+    ligar.classList.toggle('ligada');
+
+    
+    
+    
+});
